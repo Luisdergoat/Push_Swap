e7d2ad75b2722622c3f96f7a5eeed4e05ad0ed9a
Luis Unsold <lunsold@student.42heilbronn.de>
2026-01-07 12:44:12 +0100

    smal algo changes

--- src/algo/help_sorting.c
+++ src/algo/help_sorting.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   help_sorting.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lunsold <lunsold@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lunsold <lunsold@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 23:47:41 by lunsold           #+#    #+#             */
-/*   Updated: 2026/01/07 00:02:09 by lunsold          ###   ########.fr       */
+/*   Updated: 2026/01/07 11:09:17 by lunsold          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,10 @@ void help_sort_three(t_list *stack_a)
 		do_rra(stack_a);
 	if (stack_a->head->value > stack_a->head->next->value)
         do_sa(stack_a);
+}
+
+int find_min_index(t_list *stack_a)
+{
+	//funktion so if the number getting pulled is larger than every one else,
+	//the number gets in front of the lowest number.
 }
--- src/algo/sorting.c
+++ src/algo/sorting.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   new_file.c                                         :+:      :+:    :+:   */
+/*   sorting.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lunsold <lunsold@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lunsold <lunsold@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 23:00:48 by lunsold           #+#    #+#             */
-/*   Updated: 2026/01/07 00:08:03 by lunsold          ###   ########.fr       */
+/*   Updated: 2026/01/07 10:55:00 by lunsold          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,31 @@ void start_sort(t_list *stack_a, t_list *stack_b)
 		do_pb(stack_a, stack_b);
 	help_sort_three(stack_a);
 }
-//3 numbers have been sorted in 
+int find_target_in_a(t_list *stack_a, int value)
+{
+	t_node *current;
+	int index;
+	int best_value;
+	int best_index;
+
+	current = stack_a->head;
+	best_index = 0;
+	best_value = INT_MAX;
+	index = 0;
+	while (current)
+	{
+		if (current->value > value && current->value < best_value)
+		{
+			best_value = current->value;
+			best_index = index;
+		}
+		current = current->next;
+		index++;
+	}
+	if (best_value == INT_MAX)
+		best_index = find_min_index(stack_a);
+	return (best_index);
+}
 //value with best index checker
 //now push back from stack b to a the number with the least moves
 //until stack b is empty
